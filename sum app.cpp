6f6c6ed4306e41cce39f5ad2f26d3751d73254e3
akash1477 <59418905+akash1477@users.noreply.github.com>
2020-07-18 17:57:15 +0530

    Create sum app.cpp

---
+++ sum app.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b;
+cout<<"\nEnter the first number: ";
+cin>>a;
+cout<<"\nEnter the second number: ";
+cin>>b;
+cout<<"The sum is: "<< a+b;
+return 0;
+}
